617a4b364d2e999ba1d1f5fd6ca2316a5830d3c3
yk-jp <k82.y12@gmail.com>
2022-09-14 14:24:14 -0700

    finish lab13

--- Lab13/ViewController.h
+++ Lab13/ViewController.h
@@ -7,7 +7,9 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController<UITextFieldDelegate>
+
+@property (strong, nonatomic) IBOutlet UITextField *DynamicTextField;
 
 
 @end
--- Lab13/ViewController.m
+++ Lab13/ViewController.m
@@ -8,15 +8,35 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+@property(readonly) NSString* keyword;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    _keyword = @"red";
+    _DynamicTextField.delegate = self;
 }
 
+- (BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string {
+    NSRange spaceSecondToLast = [textField.text rangeOfString: @" " options: NSBackwardsSearch];
+    NSUInteger start = spaceSecondToLast.length == 0 ? 0 : spaceSecondToLast.location + 1;
+    NSRange textRange = NSMakeRange(start, range.location - start);
+    NSString *text = [textField.text substringWithRange: textRange];
+    NSDictionary *attr = @{ NSForegroundColorAttributeName : [UIColor blackColor] };
+    
+    if([[text stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceCharacterSet]] length] > 0) {
+        if([[text lowercaseString] isEqualToString: _keyword]) {
+            attr = @{ NSForegroundColorAttributeName : [UIColor redColor] };
+        }
+        
+        NSMutableAttributedString *newText = [textField.attributedText mutableCopy];
+        [newText addAttributes:attr range: textRange];
+        [textField setAttributedText: newText];
+    }
+    
+    return YES;
+}
 
 @end
